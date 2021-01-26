a12dbf4ff15c9c4ef98cdcecb9b81834d16f4c68
rachel <rng@stuy.edu>
2021-01-26 18:51:13 -0500

    extension

--- description/description.js
+++ description/description.js
@@ -1,27 +1,9 @@
 var KEY =  "881d410edc88957"
-var endpoint = "https://api.ocr.space/parse/image?apikey="+KEY;
+var ocr_endpoint = "https://api.ocr.space/parse/image?apikey="+KEY;
+var ml_endpoint = "https://greenturtle.herokuapp.com/predict"; 
 var images = document.getElementsByTagName("img");
 
-function insert_alt(image, endpoint, request_options){
-	fetch(endpoint, requestOptions)
-		.then(response => response.json())
-		.then(result => {
-			console.log(result);
-			var alt_text = result['ParsedResults'][0]['ParsedText'];
-			image.alt = alt_text;
-		})
-		.catch(error => console.log(error));
-}
-
-for(i = 0; i < images.length; i++){
-	var src = images[i].src;
-	var image_url = src;
-	if(!image_url.startsWith("http")){
-		image_url = window.location.protocol + "//" + window.location.host + src;
-	}
-	var imagenode = images[i];
-	var alt_text;
-	
+function ocr_request(endpoint){
 	var myHeaders = new Headers();
 	myHeaders.append("apikey", KEY);
 	myHeaders.append("Accept", "application/json");
@@ -43,5 +25,57 @@ for(i = 0; i < images.length; i++){
 		redirect: 'follow'
 	};
 
-	insert_alt(images[i], endpoint, requestOptions);
+	var ocr_text = fetch(endpoint, requestOptions)
+		.then(response => response.json())
+		.then(result => result)
+		.catch(error => console.log(error));
+    return ocr_text['ParsedResults'][0]['ParsedText'];
 }
+
+function ml_request(endpoint){
+    var myHeaders = new Headers();
+    myHeaders.append("Access-Control-Allow-Origin", "*");
+    myHeaders.append("Accept", "application/json");
+
+    var formdata = new FormData();
+    formdata.append("data", image_url);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        rejectUnauthorized: false
+    };
+
+	var prediction = fetch(endpoint, requestOptions)
+		.then(response => response.json())
+		.then(result => result)
+		.catch(error => console.log(error));
+    return prediction['prediction'];
+}
+
+function insert_alt(image){
+    console.log("insert alt");
+    var prediction = ml_request(ml_endpoint);
+    console.log("prediction: " + prediction);
+    var ocr_text = ocr_request(ocr_endpoint);
+    console.log("ocr: " + ocr_text);
+
+    var alt_text = prediction + ". " + ocr_text; 
+    console.log(alt_text);
+
+    image.alt = alt_text;
+}
+
+for(i = 0; i < images.length; i++){
+	var src = images[i].src;
+	var image_url = src;
+	if(!image_url.startsWith("http")){
+		image_url = window.location.protocol + "//" + window.location.host + src;
+	}
+	var imagenode = images[i];
+	
+
+	insert_alt(images[i]);
+}
+
