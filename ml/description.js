34e75416fd7e70f958da1523cb3d0972f689bb33
rachel <rng@stuy.edu>
2021-01-26 17:41:14 -0500

    ml extension

--- ml/description.js
+++ ml/description.js
@@ -1,46 +1,51 @@
-var ml_endpoint = "https://0.0.0.0:5000/predict"; 
+var ml_endpoint = "https://greenturtle.herokuapp.com/predict"; 
 var images = document.getElementsByTagName("img");
+var dev = false;
 
 function insert_alt(image, endpoint, request_options){
-    let alt_text;
+    let alt_text = "";
+    if (dev) {
+        endpoint = "https://cors-anywhere.herokuapp.com/" + endpoint;
+    }
 	fetch(endpoint,request_options)
 		.then(response => response.json())
 		.then(result => {
-			console.log(result);
-			alt_text = result['prediction'];
+			image.alt = result['prediction'];
+            console.log(image.alt)
 		})
 		.catch(error => console.log(error));
-    image.alt = alt_text;
 }
 
 for(i = 0; i < images.length; i++){
 	var src = images[i].src;
 	var image_url = src;
-	if(!image_url.startsWith("http")){
-		image_url = window.location.protocol + "//" + window.location.host + src;
-	}
-	var imagenode = images[i];
-	var alt_text;
-	
-	var myHeaders = new Headers();
-	myHeaders.append("Accept", "application/json");
-	myHeaders.append("Access-Control-Allow-Origin", "*");
+    
+    if (!images[i].alt && (image_url.includes("png") || image_url.includes("jpg"))) {
+        if(!image_url.startsWith("http")){
+            image_url = window.location.protocol + "//" + window.location.host + src;
+        }
 
-	var formdata = new FormData();
-	formdata.append("data", image_url);
+        var imagenode = images[i];
+        
+        var myHeaders = new Headers();
+        myHeaders.append("Access-Control-Allow-Origin", "*");
+        myHeaders.append("Accept", "application/json");
 
-	var requestOptions = {
-		method: 'POST',
-		headers: myHeaders,
-		mode: 'no-cors',
-		body: formdata,
-        rejectUnauthorized: false
-	};
+        var formdata = new FormData();
+        formdata.append("data", image_url);
 
-    var d = new Date();
-    var n = d.toLocaleTimeString();
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata,
+            rejectUnauthorized: false
+        };
 
-    console.log(n);
+        var d = new Date();
+        var n = d.toLocaleTimeString();
 
-	insert_alt(images[i], ml_endpoint, requestOptions);
+        console.log(n);
+
+        insert_alt(images[i], ml_endpoint, requestOptions);
+    }
 }
